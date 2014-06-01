093589975bd389c5c3fc18dcee43acb687c26047
ahmad seleem <ahmad.dev@me.com>
2014-06-01 14:53:58 -0700

    update neotree with toggle added

--- vendor/neotree/neotree-autoloads.el
+++ vendor/neotree/neotree-autoloads.el
@@ -1,8 +1,9 @@
-;;; neotree-autoloads.el --- summary
+;;; neotree-autoloads.el --- A emacs tree plugin like NerdTree for Vim
 
 ;; Copyright (C) 2014 jaypei
 
 ;; Author: jaypei <jaypei97159@gmail.com>
+;; URL: https://github.com/jaypei/emacs-neotree
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -21,12 +22,17 @@
 
 ;;; Code:
 
-;;;### (autoloads (neotree neotree-dir neotree-mode) "neotree" "neotree.el"
-;;;;;;  (21308 10187 416916 138000))
+;;;### (autoloads (neotree neotree-dir neotree-toggle neotree-mode)
+;;;;;;  "neotree" "neotree.el" (21385 31699 0 0))
 ;;; Generated autoloads from neotree.el
 
 (autoload 'neotree-mode "neotree" "\
 A major mode for displaying the directory tree in text mode.
+
+\(fn)" t nil)
+
+(autoload 'neotree-toggle "neotree" "\
+
 
 \(fn)" t nil)
 
